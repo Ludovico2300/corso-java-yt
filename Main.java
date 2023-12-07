5be4376a90ffd66c0b59d169026092f5394af909
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-07 23:59:59 +0100

    variabili

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    
+    public static void main(String[] args) {
+        int x;
+        x=5; 
+        String y = "questa è una stringa"; // insieme di caratteri, doppi apici
+        double z = 3.14;
+        boolean b = true;
+        char c = 'a'; // carattere singolo, singoli apici
+        System.out.println(z);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
