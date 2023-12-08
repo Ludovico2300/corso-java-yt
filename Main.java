4a207b4d4b8888867a3ea832acbe090c6cc0591f
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-08 12:58:30 +0100

    Math class

--- Main.java
+++ Main.java
@@ -2,17 +2,30 @@
 public class Main {
 
     public static void main(String[] args) {
-        int x = 10;
-        int y = 2;
-        int z = x + y;
-        System.out.println(z);
-        System.out.println(x + y);
-        System.out.println(x++);
-        System.out.println(x - y);
-        System.out.println(x / y);
-        System.out.println(x * y);
-        System.out.println(x % y);
-        System.out.println(x ^ y);
+
+        System.out.println(Math.abs(-55)); // output: 55
+        System.out.println(Math.min(55, 21)); // output: 21
+        System.out.println(Math.max(55, 21)); // output: 55
+        System.out.println(Math.sqrt(64)); // output: 8
+        System.out.println(Math.pow(2, 3)); // output: 8
+        System.out.println(Math.round(3.14)); // output: 3
+        System.out.println(Math.ceil(3.14)); // output: 4
+        System.out.println(Math.floor(3.14)); // output: 3
+        System.out.println(Math.random()); // output: random number between 0 and 1
+        System.out.println(Math.PI); // output: 3.141592653589793
+        System.out.println(Math.E); // output: 2.718281828459045
+        System.out.println(Math.log(Math.E)); // output: 1.0
+        System.out.println(Math.log10(100)); // output: 2.0
+        System.out.println(Math.sin(0)); // output: 0.0
+        System.out.println(Math.cos(0)); // output: 1.0
+        System.out.println(Math.tan(0)); // output: 0.0
+        System.out.println(Math.asin(0)); // output: 0.0
+        System.out.println(Math.acos(0)); // output: 1.5707963267948966
+        System.out.println(Math.atan(0)); // output: 0.0
+        System.out.println(Math.atan2(0, 0)); // output: 0.0
+        System.out.println(Math.toDegrees(0)); // output: 0.0
+        System.out.println(Math.toRadians(0)); // output: 0.0
+        System.out.println(Math.exp(0)); // output: 1.0
 
     }
 }
