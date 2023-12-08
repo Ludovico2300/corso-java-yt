caf58dfc598fdc2f0e782c55d611a541dd998f98
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-08 14:38:55 +0100

    for

--- Main.java
+++ Main.java
@@ -3,20 +3,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // ciclo while (verifico la condizione prima)
-        int i = 0;
-        while (i < 10) {
-            System.out.println("while " + i);
-            i++;
+        // ciclo for
+        for (int i = 0; i < 10; i++) {
+            System.out.println("for " + i);
         }
 
-        // ciclo do-while (verifico la condizione dopo), almeno una volta eseguo il
-        // codice
-        int j = 0;
-        do {
-            System.out.println("do-while " + j);
-            j++;
-        } while (j < 10);
+        // for annidati
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("for annidato " + i + " " + j);
+            }
+        }
+
+        // for enhanced --> sarebbe il for each
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        for (int i : array) {
+            System.out.println("for enhanced " + i);
+        }
+
+        // break and continue
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println("for break " + i);
+        }
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                continue;
+            }
+            System.out.println("for continue " + i);
+        }
 
     }
 }
