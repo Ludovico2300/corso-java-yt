21fb193d08dc9001d0d6efbdde770c01da0fbe70
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-08 13:43:43 +0100

    operatori di comparazione

--- Main.java
+++ Main.java
@@ -3,30 +3,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // operatori di comparazione:
-        // == uguale a
-        // != diversa da
-        // > maggiore di
-        // < minore di
-        // >= maggiore o uguale a
-        // <= minore o uguale a
+        // operatori logici
+        // && (and)
+        // || (or)
+        // ! (not)
 
-        int a = 5;
-        int b = 10;
-        boolean c = a == b;
-        boolean d = a != b;
-        boolean e = a > b;
-        boolean f = a < b;
-        boolean g = a >= b;
-        boolean h = a <= b;
-        System.out.println(c); // output: false
-        System.out.println(d); // output: true
-        System.out.println(e); // output: false
-        System.out.println(f); // output: true
-        System.out.println(g); // output: false
-        System.out.println(h); // output: true
-        System.out.println(h == f); // output: true
-        System.out.println(h == g); // output: false
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        boolean result = a > b && b < c;
+        System.out.println(result); // output: false
+
+        boolean result2 = a > b || b < c;
+        System.out.println(result2); // output: true
+
+        boolean result3 = !result;
+        System.out.println(result3); // output: true
 
     }
 }
