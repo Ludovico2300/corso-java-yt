dc6d02a820f8ac3d6a66277fa0b2de19ec9acb17
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-08 13:05:19 +0100

    operatori di comparazione

--- Main.java
+++ Main.java
@@ -3,29 +3,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(Math.abs(-55)); // output: 55
-        System.out.println(Math.min(55, 21)); // output: 21
-        System.out.println(Math.max(55, 21)); // output: 55
-        System.out.println(Math.sqrt(64)); // output: 8
-        System.out.println(Math.pow(2, 3)); // output: 8
-        System.out.println(Math.round(3.14)); // output: 3
-        System.out.println(Math.ceil(3.14)); // output: 4
-        System.out.println(Math.floor(3.14)); // output: 3
-        System.out.println(Math.random()); // output: random number between 0 and 1
-        System.out.println(Math.PI); // output: 3.141592653589793
-        System.out.println(Math.E); // output: 2.718281828459045
-        System.out.println(Math.log(Math.E)); // output: 1.0
-        System.out.println(Math.log10(100)); // output: 2.0
-        System.out.println(Math.sin(0)); // output: 0.0
-        System.out.println(Math.cos(0)); // output: 1.0
-        System.out.println(Math.tan(0)); // output: 0.0
-        System.out.println(Math.asin(0)); // output: 0.0
-        System.out.println(Math.acos(0)); // output: 1.5707963267948966
-        System.out.println(Math.atan(0)); // output: 0.0
-        System.out.println(Math.atan2(0, 0)); // output: 0.0
-        System.out.println(Math.toDegrees(0)); // output: 0.0
-        System.out.println(Math.toRadians(0)); // output: 0.0
-        System.out.println(Math.exp(0)); // output: 1.0
+        // operatori di comparazione:
+        // == uguale a
+        // != diversa da
+        // > maggiore di
+        // < minore di
+        // >= maggiore o uguale a
+        // <= minore o uguale a
+
+        int a = 5;
+        int b = 10;
+        boolean c = a == b;
+        boolean d = a != b;
+        boolean e = a > b;
+        boolean f = a < b;
+        boolean g = a >= b;
+        boolean h = a <= b;
+        System.out.println(c); // output: false
+        System.out.println(d); // output: true
+        System.out.println(e); // output: false
+        System.out.println(f); // output: true
+        System.out.println(g); // output: false
+        System.out.println(h); // output: true
+        System.out.println(h == f); // output: true
+        System.out.println(h == g); // output: false
 
     }
 }
