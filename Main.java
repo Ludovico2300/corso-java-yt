119e7fcf9aceeb2529e14ed123f198f971f9709d
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-09 18:46:52 +0100

    metodi

--- Main.java
+++ Main.java
@@ -1,61 +1,24 @@
-
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("INIZIO ARRAY LIST");
+        cucinaPranzo();
+        int totaleSpeso = addizione(20, 3);
+        System.out.println(totaleSpeso);
+    }
 
-        // ARRAY LIST --> accettano sono references
-        ArrayList<String> persone = new ArrayList<>();
-        persone.add("Mario");
-        persone.add("Luigi");
-        persone.add("Giovanni");
-        persone.add("Marco");
+    static void cucinaPranzo() {
+        Scanner tastiera = new Scanner(System.in);
+        System.out.println("Cosa vuoi cucinare?");
+        String cibo = tastiera.nextLine();
+        System.out.println("sto cucinando " + cibo.toLowerCase());
+        tastiera.close();
+    }
 
-        // mandare a schermo
-        System.out.println(persone.get(0));
-
-        for (int i = 0; i < persone.size(); i++) {
-            System.out.println(persone.get(i));
-        }
-
-        for (String persona : persone) {
-            System.out.println(persona);
-        }
-
-        // modificare elemento
-        persone.set(0, "Mario Rossi");
-
-        // rimuovere elemento
-        persone.remove(0);
-        persone.clear();
-
-        // arrayList 2d
-        ArrayList<ArrayList<String>> classi = new ArrayList<>();
-
-        ArrayList<String> persone1 = new ArrayList<>();
-        persone1.add("Mario");
-        persone1.add("Alex");
-        persone1.add("Giovanni");
-        persone1.add("Marco");
-
-        ArrayList<String> persone2 = new ArrayList<>();
-        persone2.add("Mario");
-        persone2.add("Luigi");
-        persone2.add("Giovanni");
-        persone2.add("Pippo");
-
-        classi.add(persone1);
-        classi.add(persone2);
-
-        for (ArrayList<String> classe : classi) {
-            System.out.println();
-            for (String persona : classe) {
-                System.out.print(persona + " ");
-            }
-        }
-
+    static int addizione(int a, int b) {
+        int risultato = a + b;
+        return risultato;
     }
 
 }
