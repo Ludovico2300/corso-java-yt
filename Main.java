02446a9b7b417a76675268176ca231bb412581fb
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-09 14:37:23 +0100

    array 2d

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("INIZIO ARRAY");
 
         // array, crearne uno, associare elementi, metodo to string, lunghezza,
         // modificare e mostrare elementi
@@ -37,5 +38,44 @@ public class Main {
         for (int number : numbers2) {
             System.out.println("ciclo for each " + number);
         }
+
+        System.out.println("INIZIO ARRAY MULTIDIMENSIONALI");
+
+        // array multidimensionale
+        String[][] classi = new String[3][3];
+
+        // associare elementi array multidimensionale
+        classi[0][0] = "Luca";
+        classi[0][1] = "Anna";
+        classi[0][2] = "Marco";
+
+        classi[1][0] = "Lucia";
+        classi[1][1] = "Alessia";
+        classi[1][2] = "Pippo";
+
+        classi[2][0] = "Mario";
+        classi[2][1] = "Gianni";
+        classi[2][2] = "Paolo";
+
+        System.out.println(classi[2][1]);
+
+        // array 2D e ciclo foR
+        for (int classe = 0; classe < classi.length; classe++) {
+            System.out.println();
+
+            for (int alunno = 0; alunno < classi[classe].length; alunno++) {
+                System.out.print(classi[classe][alunno] + " ");
+            }
+        }
+        System.out.println();
+        // array 2D e ciclo for-each
+        for (String[] classe : classi) {
+            System.out.println();
+
+            for (String alunno : classe) {
+                System.out.print(alunno + " ");
+            }
+        }
+
     }
 }
