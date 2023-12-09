9a3d599fb2ecfbdc081afeca7d293ff6860b3f8f
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-09 19:09:16 +0100

    introduzione OOB

--- Main.java
+++ Main.java
@@ -2,30 +2,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        int totaleSpeso = addizione(20, 3);
-        int totaleSpeso2 = addizione(20, 3, 5);
-        double totaleSpesoDouble = addizione(22.4, 3.7);
-        System.out.println(totaleSpeso);
-        System.out.println(totaleSpeso2);
-        System.out.println(totaleSpesoDouble);
-    }
+        Persona Luca = new Persona();
+        Luca.saluta();
+        Luca.colorePreferito = "Rosso";
+        System.out.println(Luca.colorePreferito);
 
-    static int addizione(int a, int b) {
-        int risultato = a + b;
-        return risultato;
-    }
-
-    static int addizione(int a, int b, int c) {
-        int risultato = a + b + c;
-        return risultato;
-    }
-
-    static double addizione(double a, double b) {
-        double risultato = a + b;
-        return risultato;
     }
 
 }
-
-// overloaded methods: un metodo con stesso nome ma parametri diversi (tipi,
-// numero)
---
+++ Persona.java
@@ -0,0 +1,16 @@
+public class Persona {
+
+    String nome = "Luca";
+    String cognome = "Rossi";
+    int eta = 25;
+    String colorePreferito = "Blu";
+
+    void saluta() {
+        System.out.println("Ciao sono " + nome + " " + cognome + " e ho " + eta + " anni.");
+    }
+
+    void cammina() {
+        System.out.println("Sto camminando");
+    }
+
+}
