4b2f92d6c331cfce4bc484d9a2323bea1d72cb49
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-12-17 15:14:49 +0100

    data e ora

--- Pacchetto1/Main.java
+++ Pacchetto1/Main.java
@@ -1,24 +1,30 @@
 package Pacchetto1;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
 
     public static void main(String[] args) {
-        // casting --> conversione di un tipo in un altro
-        // implicito
+        // data e ora
 
-        int a = 10;
-        double b = a;
-        System.out.println(b);
-        System.out.println(a);
-        System.out.println("----------------------");
+        LocalDate x = LocalDate.now();
+        System.out.println(x);
+        System.out.println(x.getDayOfMonth());
+        System.out.println(x.getMonth());
+        System.out.println(x.getYear());
+        LocalTime y = LocalTime.now();
+        System.out.println(y);
+        LocalDateTime z = LocalDateTime.now();
+        System.out.println(z);
 
-        // esplcito (quando passo da un tipo di dato ampio a uno più ristretto)
+        // formattare
 
-        double c = 10.5;
-        int d = (int) c;
-        System.out.println(d);
-        System.out.println(c);
-        System.out.println("----------------------");
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String data = x.format(format);
+        System.out.println(data);
 
     }
 
