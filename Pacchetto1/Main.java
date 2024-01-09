e0aed3ed84237d8b01a612e4cd6f505dae954ec5
Ludovico Colucci <ludovicocolucci@gmail.com>
2024-01-09 17:13:25 +0100

    fix

--- Pacchetto1/Main.java
+++ Pacchetto1/Main.java
@@ -7,14 +7,15 @@ public class Main {
     public static void main(String[] args) {
         // modificatori di accesso (public, private, protected, default)
 
-        /*
+        /**
          * 
-         * class package subclass world
+         * -----------------class package subclass world
          * 
-         * public si si si si
-         * protected si si si no
-         * default si si no no
-         * private si no no no
+         * public-----------si------si------si------si
+         * protected--------si------si------si------no
+         * default----------si------si------no------no
+         * private----------si------no------no------no
+         * *assenza*
          * 
          */
 
